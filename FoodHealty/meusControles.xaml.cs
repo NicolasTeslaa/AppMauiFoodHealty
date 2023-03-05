27cc526e3001fcee7fb2ce084be8286ce3390c30
Nicolas Tesla <65693466+NicolasTeslaa@users.noreply.github.com>
2023-03-05 16:12:25 -0300

    atts

--- FoodHealty/meusControles.xaml.cs
+++ FoodHealty/meusControles.xaml.cs
@@ -3,7 +3,7 @@ namespace FoodHealty;
 public partial class meusControles : ContentView
 {
 	public static readonly BindableProperty TitleTextProperty = BindableProperty.Create(nameof(TitleText),
-		typeof(string), typeof(meusControles));
+		typeof(string), typeof(meusControles), defaultValue: string.Empty, defaultBindingMode: BindingMode.OneWay,);
 
 	public string TitleText { get; set; }
 
